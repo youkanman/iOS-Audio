613493977154fabc529a8907ee80d0b3b70cdc7a
youkanman <youkan.newtype@gmail.com>
2020-07-10 13:25:13 +0900

    add playing complete callback

--- iOS-AudioTest/ViewController.swift
+++ iOS-AudioTest/ViewController.swift
@@ -10,55 +10,102 @@ import UIKit
 import AudioKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var button: UIButton!
     @IBOutlet weak var buttonAudio: UIButton!
     @IBOutlet weak var labelGain: UILabel!
     @IBOutlet weak var sliderGain: UISlider!
     
-    var sliderValue: Int = 0;
+    var sliderValue: Int = 0
     
+    let wavList = ["output02.wav", "output00.wav", "output01.wav"]
+    //let wavList = ["output02.wav"]
+
+    // Audioの再生
+    var players = [AKAudioPlayer]()
+    var currentPlayer: AKAudioPlayer?
+    var currentPlayerIndex: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         sliderGain.value = Float(sliderValue)
         labelGain.text = String(sliderValue)
+
+        // create audio player
+        players = getPlayers()
     }
 
-    @IBAction func displayMessage(_ sender: Any) {
-        label.text = "Hello World."
-    }
-    
-    //@IBAction func gainValue(_ sender: Any) {
-    //    let sliderValue:Int = Int(sender.value)
-    //    labelGain.text = String(sliderValue)
-    //}
-    
     @IBAction func gainValue(_ sender: UISlider, forEvent event: UIEvent) {
         sliderValue = Int(sender.value)
         labelGain.text = String(sliderValue)
     }
 
     @IBAction func playAudio(_ sender: Any) {
-        label.text = "Play Audio."
-     
+        
+        if currentPlayer?.isPlaying ?? false {
+            print("Playing...")
+            return
+        }
+
+        // 1セット目再生
+        buttonAudio.isEnabled = false
+        playAudio()
+    }
+    
+    private func playAudio() {
+        currentPlayer = players[currentPlayerIndex]
+        
         do {
-            var file = try AKAudioFile(readFileName: "output02.wav")
-            var player = try AKAudioPlayer(file: file)
             // Define your gain below. >1 means amplifying it to be louder
             //let booster = AKBooster(player, gain: 1.3)
-            var booster = AKBooster(player, gain: Double(sliderGain.value))
+            let booster = AKBooster(currentPlayer, gain: Double(sliderGain.value))
             AudioKit.output = booster
-            try AudioKit.start()
-            // And then to play your file:
-            player.play()
 
-            label.text = "Stop Audio."
+            try AudioKit.start()
+
+            currentPlayer?.play()
+            print(currentPlayer?.isPlaying ?? false)
+            
         } catch {
             // Log your error
         }
     }
+
+    private func getPlayers() -> [AKAudioPlayer] {
+        var players = [AKAudioPlayer]()
+        
+        wavList.forEach { wav in
+            do {
+                let file = try AKAudioFile(readFileName: wav)
+                let player = try AKAudioPlayer(file: file, completionHandler: didFinishPlaying)
+                players.append(player)
+            } catch{
+            }
+        }
+        
+        return players
+    }
+
+    private func didFinishPlaying() {
+        currentPlayer?.stop()
+        print(currentPlayer?.isPlaying ?? false)
+        
+        do {
+            try AudioKit.stop()
+        } catch {
+            // error
+        }
+        // 2セット目以降
+        currentPlayerIndex += 1
+        
+        if currentPlayerIndex > players.count - 1 {
+            print("done.")
+            currentPlayerIndex = 0
+            buttonAudio.isEnabled = true
+
+            return
+        }
+        
+        playAudio()
+    }
 }
-
-
